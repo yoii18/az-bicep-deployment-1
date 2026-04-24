5cd7a8987643ef5cfbdbb7fdbe18067ac5195f3e
yoii18 <dpssalhotra@gmail.com>
2026-04-25 01:12:49 +0530

    we try again lads

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -17,4 +17,9 @@ az storage account create \
     --resource-group "$TFSTATE_RG_NAME" \ 
     --sku Standard_LRS \
     --min-tls-version TLS1_2
-    
+
+echo ">>> Creating state containers (one per environment)..."
+az storage container create --name "staging" --account-name "$STORAGE_ACCOUNT"
+az storage container create --name "prod" --account-name "$STORAGE_ACCOUNT"
+
+echo ">>> Creating staging Service Principal..."
