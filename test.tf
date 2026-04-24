8c2c15e0c06ab4b167c4a6287626942b69d333bb
yoii18 <dpssalhotra@gmail.com>
2026-04-24 17:15:05 +0530

    pushing to prod

--- test.tf
+++ test.tf
@@ -25,6 +25,7 @@ resource "azurerm_storage_account" "strgAcct" {
   resource_group_name      = azurerm_resource_group.resourceGroup.name
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  is_hns_enabled           = true
 
   tags = {
     environment = "DEV"
