b7199e9706070398a541d5f56025b980a576c4c4
yoii18 <dpssalhotra@gmail.com>
2026-04-28 10:21:25 +0530

    x3

--- test.tf
+++ test.tf
@@ -1,35 +1,35 @@
-terraform {
-  required_version = ">= 1.8, < 2.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">1.9.1"
-    }
-  }
-}
+# terraform {
+#   required_version = ">= 1.8, < 2.0"
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = ">1.9.1"
+#     }
+#   }
+# }
 
-provider "azurerm" {
-  features {
+# provider "azurerm" {
+#   features {
 
-  }
-}
+#   }
+# }
 
-resource "azurerm_resource_group" "resourceGroup" {
-  name     = "arm-introduction-02-cli"
-  location = "Japan East"
-}
+# resource "azurerm_resource_group" "resourceGroup" {
+#   name     = "arm-introduction-02-cli"
+#   location = "Japan East"
+# }
 
-resource "azurerm_storage_account" "strgAcct" {
-  name                     = "tfstrgacctabcd101"
-  location                 = azurerm_resource_group.resourceGroup.location
-  resource_group_name      = azurerm_resource_group.resourceGroup.name
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  is_hns_enabled           = true
+# resource "azurerm_storage_account" "strgAcct" {
+#   name                     = "tfstrgacctabcd101"
+#   location                 = azurerm_resource_group.resourceGroup.location
+#   resource_group_name      = azurerm_resource_group.resourceGroup.name
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+#   is_hns_enabled           = true
 
-  tags = {
-    environment = "DEV"
-  }
-}
+#   tags = {
+#     environment = "DEV"
+#   }
+# }
 
 
