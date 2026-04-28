e1bb1748c374483b68b900fcbef4397c69342d15
yoii18 <dpssalhotra@gmail.com>
2026-04-28 10:40:03 +0530

    x9

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-resource "azure_resource_group" "main" {
+resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azure_data_factory" "adf" {
+resource "azurerm_data_factory" "adf" {
   name                = "adf-${var.resource_group_name}"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
